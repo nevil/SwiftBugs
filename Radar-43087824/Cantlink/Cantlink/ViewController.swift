3c767058231ec84df905cd721fe3974974f6a63d
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2018-08-09 19:06:41 +0900

    Radar 43087824
    Fails to link with Crashlytics

---
+++ Radar-43087824/Cantlink/Cantlink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cantlink
+//
+//  Created by Anders Hasselqvist on 2018/08/09.
+//  Copyright © 2018 Anders Hasselqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
