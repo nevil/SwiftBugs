451a90931d5b446015bf0ed8c2c8a626095abc45
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2018-09-19 11:41:31 +0900

    Update to use Xcode 10

--- verify.sh
+++ verify.sh
@@ -18,17 +18,17 @@ function execswift() {
 }
 
 execswift SR-7055.swift -c -v
-execswift SR-7235.swift -sdk /Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -target arm64-apple-ios10.0 -c -v
-execswift SR-7295.swift -sdk /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+execswift SR-7235.swift -sdk /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -target arm64-apple-ios10.0 -c -v
+execswift SR-7295.swift -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 execswift SR-7425.swift -c -v
-execswift SR-7986.swift -sdk /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+execswift SR-7986.swift -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 
 pushd SR-7784
 execswift SR-7784.swift -c -F . -framework VersionedKit.framework
 popd
 
 bugstart Radar-41712912.swift
-IOS_SDK="/Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
+IOS_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
 IOS_TARGET="arm64-apple-ios12.0"
 
 $SWIFTC -sdk $IOS_SDK -target $IOS_TARGET -c Radar-41712912.swift
