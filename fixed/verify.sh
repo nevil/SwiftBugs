0e7cc0b5104c7d2a5bd5064628eba128bc779936
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2019-04-19 15:37:31 +0900

    Radar 41712912 fixed with iOS 12.2 SDK

--- fixed/verify.sh
+++ fixed/verify.sh
@@ -14,6 +14,13 @@ IOS_TARGET="arm64-apple-ios12.0"
 $SWIFTC -frontend -sdk $IOS_SDK -target $IOS_TARGET -c -update-code -primary-file SR-8150.swift -emit-migrated-file-path SR-8150.migrated.swift -swift-version 4
 execswift SR-8150.migrated.swift -c -v -sdk $IOS_SDK -target $IOS_TARGET
 
+bugstart Radar-41712912.swift
+IOS_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
+IOS_TARGET="arm64-apple-ios12.0"
+
+$SWIFTC -sdk $IOS_SDK -target $IOS_TARGET -c Radar-41712912.swift
+bugend Radar-41712912.swift
+
 bugstart Radar-43088982.swift
 $SWIFTC -frontend -sdk $IOS_SDK -target $IOS_TARGET -c -update-code -primary-file Radar-43088982.swift -emit-migrated-file-path Radar-43088982.migrated.swift -swift-version 5
 diff Radar-43088982.swift Radar-43088982.migrated.swift && echo "Verified." || echo "Failed."
