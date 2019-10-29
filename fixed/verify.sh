22d3d34a3697fee243dc57f085115ffe7862940a
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2019-10-29 10:55:37 +0900

    SR-7295 has been fixed

--- fixed/verify.sh
+++ fixed/verify.sh
@@ -3,10 +3,18 @@
 # Invoke this script as:
 # SWIFTC=/Library/Developer/Toolchains/swift-4.2-DEVELOPMENT-SNAPSHOT-2018-06-26-a.xctoolchain/usr/bin/swiftc ./verify.sh
 
-function execswift() {
+function bugstart() {
     echo "################## Start $1 #############################################################"
-    $SWIFTC ${@:2} $1
+}
+
+function bugend() {
     echo "################## End $1 ###############################################################"
+}
+
+function execswift() {
+    bugstart $1
+    $SWIFTC ${@:2} $1
+    bugend $1
 }
 
 IOS_SDK="/Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
@@ -25,4 +33,6 @@ bugstart Radar-43088982.swift
 $SWIFTC -frontend -sdk $IOS_SDK -target $IOS_TARGET -c -update-code -primary-file Radar-43088982.swift -emit-migrated-file-path Radar-43088982.migrated.swift -swift-version 5
 diff Radar-43088982.swift Radar-43088982.migrated.swift && echo "Verified." || echo "Failed."
 bugend Radar-43088982.swift
+
+execswift SR-7295.swift -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 
